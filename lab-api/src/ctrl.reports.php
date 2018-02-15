1fc9d3c243f985fc45ce08bc17100db840df3305
Julius Alvarado <javascript.uiux@gmail.com>
2018-02-15 13:33:21 -0800

    got 'V2' flatfile data parsing algorithm to successfully store all cells with numbers all well

--- lab-api/src/ctrl.reports.php
+++ lab-api/src/ctrl.reports.php
@@ -9,22 +9,26 @@
 $action = isset($_GET["action"]) ? $_GET["action"] : null;
 $dataReport1 = scrapeReport1();
 
-if($action === 'show-report') {
+if ($action === 'info') {
+    phpinfo();
+}
+
+if ($action === 'show-report') {
     print_r($dataReport1);
 }
 
-if($action === 'test1') {
+if ($action === 'test1') {
     testGetReport($dataReport1);
 }
 
-if($action === 'gcloud-create-report1') {
+if ($action === 'gcloud-create-report1') {
     $model = $app['report.model']($app);
     $labReportId = $model->createGetReport($dataReport1);
     echo "lab GetReport creation id = $labReportId";
 }
 
 function testGetReport($reports) {
-    for($row = 0; $row < count($reports); $row++) {
+    for ($row = 0; $row < count($reports); $row++) {
         echo "<p><b>Row Number $row:</b></p>";
         echo "<ol>";
         $col = 0;
@@ -39,23 +43,44 @@ function testGetReport($reports) {
 function scrapeReport1() {
     $report1 = file_get_contents("http://lab916.wpengine.com/mws/src/MarketplaceWebService/api/report1.php");
     $explode1 = explode('<h2>Report Contents</h2>', $report1);
-    $explode2b = explode('  ', $explode1[1]);
+    $cells = explode("\t", $explode1[1]);
 
-    // ---------- $rows does work ----------
-    $rows = explode("\n", $explode2b[0]);
-
-    $amazonRowsFba = [];
-    for ($i=0; $i<count($rows); $i++) {
-        $row = preg_replace('/\t/', '|', $rows[$i]);
-        $rowArray = explode('|', $row);
-        $amazonRowsFba[$i] = $rowArray;
+    $table = [];
+    $row = 0;
+    $curRow = [];
+    for ($i = 0; $i < count($cells); $i++) {
+        $cel = $cells[$i];
+        if (strpos($cells[$i], "\r\n") !== false) {
+            $newRowAR = explode("\r\n", $cel);
+            array_push($curRow, $newRowAR[0]);
+            $table[$row] = $curRow;
+            $curRow = [];
+            $row++;
+            $table[$row] = $curRow;
+            array_push($curRow, $newRowAR[1]);
+        } else {
+            array_push($curRow, $cel);
+        }
     }
 
-    $amazonRowsFbaClean = []; $idx = 0;
-    for ($i = 0; $i<count($amazonRowsFba); $i++) {
+    /* -- old stuff --
+        // deals with strings
+        $rows = []; $amazonRowsFba = [];
+        for ($i=0; $i<count($rows); $i++) {
+            $row = preg_replace("/\t/", "|", $rows[$i]);
+            $rowArray = explode("|", $row);
+            $amazonRowsFba[$i] = $rowArray;
+        }
+
+
+    */
+
+    $amazonRowsFbaClean = [];
+    $idx = 0;
+    for ($i = 0; $i < count($table); $i++) {
         $tempA = array();
-        foreach ($amazonRowsFba[$i] as $record) {
-            if(str_word_count($record) > 0) {
+        foreach ($table[$i] as $record) {
+            if ((str_word_count($record) > 0) or (1 === preg_match('~[0-9]~', $record))) {
                 $tempA[$idx] = $record;
             }
             $idx++;

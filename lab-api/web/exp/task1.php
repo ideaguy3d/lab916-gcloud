b1938648da0e6994fbb6ff73fd32f99345b335da
Julius Alvarado <javascript.uiux@gmail.com>
2018-04-10 11:30:52 -0700

    ugh, last commit didn't get my new 'exp' folder which will be used for code experimenting to figure stuff out

---
+++ lab-api/web/exp/task1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Julius Alvarado.
+ * User: Lab916
+ * Date: 4/10/2018
+ * Time: 10:45 AM
+ */
+
+$x1 = 255;
+$x2 = 45;
+
+echo "The computation = " . ($x1 + $x2) . " data records processed.";

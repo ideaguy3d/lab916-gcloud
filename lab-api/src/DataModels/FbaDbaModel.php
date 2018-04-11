a8da6fb30c09944ff31480c67b1dd22a5b01fa01
Julius Alvarado <javascript.uiux@gmail.com>
2018-04-11 16:06:25 -0700

    trying to figure out how to group rows with same amazon_order_id

--- lab-api/src/DataModels/FbaDbaModel.php
+++ lab-api/src/DataModels/FbaDbaModel.php
@@ -15,6 +15,11 @@ class FbaDbaModel implements FbaDbaInterface
     private $dsn;
     private $user;
     private $password;
+    // utility count for tracking nested functions
+    private  $count;
+    // will be used to get distinct `amazon_order_id` rows
+    private $distinctTableRows;
+
 
     public function __construct($dsn, $user, $password) {
         $this->dsn = $dsn;
@@ -35,19 +40,35 @@ class FbaDbaModel implements FbaDbaInterface
         $statement = $pdo->prepare($query);
         $statement->execute();
 
-        $rows = [];
-
+        // get ALL of the rows in the db
+        $tableRows = [];
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-            array_push($rows, $row);
+            array_push($tableRows, $row);
         }
 
-        echo "break point here";
+        $queryDistinct = "SELECT DISTINCT amazon_order_id from $tableName";
+        $statementDistinct = $pdo->prepare($queryDistinct);
+        $statementDistinct->execute();
+
+        // get distinct amazon_order_id rows
+        $this->distinctTableRows = [];
+        while($row = $statementDistinct->fetch(PDO::FETCH_ASSOC)) {
+            array_push($distinctTableRows, $row);
+        }
+
+        $this->count = 0;
+        $groupedAmazonOrders = array_filter($tableRows, function ($val, $key) {
+            echo $val == $this->distinctTableRows[$this->count]['amazon_order_id'];
+            $this->count++;
+        }, ARRAY_FILTER_USE_BOTH);
+
+        echo "break point here, $groupedAmazonOrders";
 
         return 1;
     }
 
     public static function getMysqlDsn($dbName, $port, $connectionName = null) {
-        if($connectionName) {
+        if ($connectionName) {
             return sprintf('mysql:unix_socket=/cloudsql/%s;dbname=%s',
                 $connectionName,
                 $dbName
